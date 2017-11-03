920378b3092fb46035d3160081db924217294590
lslx <zz00zz.9774@gmail.com>
2017-11-03 13:54:41 +0800

    vim and bash config setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp .vimrc ~/.vimrc
+cp .bash_profile_port ~/.bash_profile_port
+echo '. ~/.bash_profile_port'>>~/.profile
+echo '. ~/.bash_profile_port'>>~/.bashrc
+# this not work , because can not export use this form
+. ~/.bash_profile_port
