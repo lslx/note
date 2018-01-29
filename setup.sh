3ccc63133d72b2b84aecfd2cd3b3383f4009f8a6
lslx <zz00zz.9774@gmail.com>
2018-01-29 13:55:57 +0800

    remove setup.sh command

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 cp .vimrc ~/.vimrc
 cp .bash_profile_port ~/.bash_profile_port
-echo '. ~/.bash_profile_port'>>~/.profile
-echo '. ~/.bash_profile_port'>>~/.bashrc
+#echo '. ~/.bash_profile_port'>>~/.profile
+#echo '. ~/.bash_profile_port'>>~/.bashrc
 # this not work , because can not export use this form
 # . ~/.bash_profile_port
